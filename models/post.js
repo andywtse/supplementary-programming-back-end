bdaac200c5220e73f4af19e6f4a6a863274b1804
nathanth <nathan.hardenburg@icloud.com>
2022-07-25 16:07:45 -0500

    delete && checkpoint

---
+++ models/post.js
@@ -0,0 +1,23 @@
+import mongoose from 'mongoose'
+
+const Schema = mongoose.Schema
+
+const replySchema = new Schema({
+  content: String,
+  author: {type: mongoose.Schema.Types.ObjectId, ref: 'Profile' }
+}, {
+  timestamps: true
+})
+
+const postSchema = new Schema({
+  title: String,
+  content: String,
+  replies: [replySchema],
+  author: {type: mongoose.Schema.Types.ObjectId, ref: "Profile"},
+}, {
+  timestamps: true
+})
+
+const Post = mongoose.model('Post', postSchema)
+
+export { Post }
